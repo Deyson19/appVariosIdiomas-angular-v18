a4db3f0d1771927bec5dca3f65c8cae42b187aa2
Deyson Venté <deiandresvent@hotmail.com>
2024-10-12 13:44:49 -0500

    logica para cambiar el idioma del sitio.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  effect,
   inject,
+  OnChanges,
+  OnDestroy,
   OnInit,
+  SimpleChanges,
 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
@@ -16,7 +20,7 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   styleUrl: './app.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy, OnChanges {
   private readonly translate = inject(TranslateService);
   private currentLang: string = 'es';
   constructor() {
@@ -29,12 +33,31 @@ export class AppComponent implements OnInit {
         : 'es';
     }
   }
+  ngOnChanges(changes: SimpleChanges): void {
+    effect(() => {
+      console.log('Cambios:', changes);
+      console.log('Usando effect');
+    });
+    console.log('Algo cambió en el componente.');
+  }
+  ngOnDestroy(): void {
+    localStorage.setItem('language', this.currentLang);
+  }
   ngOnInit(): void {
+    this.currentLang = this.checkCurrentLang() || 'es';
     this.translate.use(this.currentLang);
   }
 
   changeLang(lang: string): void {
-    this.translate.use(lang);
+    const myLang = this.checkCurrentLang();
+    if (myLang !== lang) {
+      this.currentLang = lang;
+      localStorage.setItem('language', lang);
+      this.translate.use(lang);
+    }
+  }
+  checkCurrentLang() {
+    return localStorage.getItem('language');
   }
   idiomas: MyLanguages[] = [
     { alias: 'es', name: 'Español' },
